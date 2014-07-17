1a107d0ae2de11b10b957b784c8086cb892c2761
Devin Dreszer <devindreszer@gmail.com>
2014-07-17 11:17:34 -0400

    Players can play a song

--- app/assets/javascripts/Player.js
+++ app/assets/javascripts/Player.js
@@ -1,1 +1,12 @@
 // Player class needs to be defined here!
+function Player(isPlaying, currentSong) {
+  this.isPlaying = isPlaying || false;
+  this.currentSong = currentSong;
+}
+
+Player.prototype = {
+  play: function(song){
+    this.isPlaying = true;
+    this.currentSong = song;
+  }
+};
